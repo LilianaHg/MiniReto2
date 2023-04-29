5eaddedbd4234bd33509f0f947ba91c4563156b5
JavierP08 <A01662438@tec.mx>
2023-04-29 07:37:36 -0600

    agregar dos componentes

--- mini-reto/client/src/App.js
+++ mini-reto/client/src/App.js
@@ -1,31 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import compoent1 from './components/component1';
 
 import React from "react";
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-    .then((res) => res.json())
-    .then((data) => setData(data.message));
-    }, []);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <compoent1>
+
+        </compoent1>
     </div>
   );
 }
