8130c844eab4ef7f89ff2deb87d125655fc91bc2
JavierP08 <A01662438@tec.mx>
2023-04-30 17:22:06 -0600

    Agregar pagina de productos

--- mini-reto/client/src/App.jsx
+++ mini-reto/client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Components/Header'
+import Productos from './Components/Productos'
 
 const App = () => {
   return (
-    <div>
-      <Header></Header>
+    <div className="">
+        <Header></Header>
+        <Productos/>
     </div>
   )
 }
